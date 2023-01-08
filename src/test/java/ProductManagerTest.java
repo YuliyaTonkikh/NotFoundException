4181eb291b4cece7350646338418e5a5ae7c5b09
Yuliya Tonkikh <uliya_i@bk.ru>
2023-01-08 18:26:33 +0700

    fix ignore rules. fix tests

---
+++ src/test/java/ProductManagerTest.java
@@ -0,0 +1,155 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class ProductManagerTest {
+    @Test
+    public void testAdd(){
+        ProductRepository repository = new ProductRepository();
+        ProductManedger manedger = new ProductManedger(repository);
+        Book book1 = new Book(1, "Lord of the Rings 1", 2_000, "", "");
+        Book book2 = new Book(1, "Lord of the Rings 2", 2_500, "", "");
+        Book book3 = new Book(1, "Lord of the Rings 3", 2_600, "", "");
+
+        manedger.add(book1);
+        manedger.add(book2);
+        manedger.add(book3);
+
+        Product[] actual = repository.findAll();
+        Product[] expected = {book1, book2, book3};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+    @Test
+    public void testAdd1(){
+        ProductRepository repository = new ProductRepository();
+        ProductManedger manedger = new ProductManedger(repository);
+        Smartphone smartphone1 = new Smartphone(1, "Iphone", 50_000, "");
+        Smartphone smartphone2 = new Smartphone(1, "Lenovo", 30_000,"");
+        Smartphone smartphone3 = new Smartphone(1, "Samsung", 40_000, "");
+
+        manedger.add(smartphone1);
+        manedger.add(smartphone2);
+        manedger.add(smartphone3);
+
+        Product[] actual = repository.findAll();
+        Product[] expected = {smartphone1, smartphone2, smartphone3};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+    @Test
+    public void test3() {
+        Smartphone smartphone = new Smartphone(1,"Lenovo", 30_000, "Lenovo");
+
+        String expected = "Lenovo";
+        String actual = smartphone.getManufacturer();;
+
+        Assertions.assertEquals(expected,actual);
+
+    }
+    @Test
+    public void test4(){
+        Book book = new Book(2, "state councilor", 200, "Borise Acunine", "");
+        String expected = "Borise Acunine";
+        String actual = book.getAutor();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void test5(){
+        Product product = new Product(1, "Book", 200, "");
+        String expected = "Book";
+        String actual = product.getTitle();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void test6(){
+        Product product = new Product(1, "Book", 200,"");
+        int expected = 200;
+        int actual = product.getCost();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void test7(){
+        Product product = new Product(1, "Book", 200, "xxx");
+        String expected = "xxx";
+        String actual = product.getName();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void test8(){
+        Product tmp = new Product(1, "nnn",300, "...");
+        String expected = "...";
+        String actual = "...";
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void test9(){
+        ProductRepository repository = new ProductRepository();
+        ProductManedger manedger = new ProductManedger(repository);
+        Book book1 = new Book(1, "Lord of the Rings 1", 2_000, "", "");
+        Book book2 = new Book(1, "Lord of the Rings 2", 2_500, "", "");
+        Book book3 = new Book(1, "Rings", 2_600, "", "");
+        Smartphone smartphone = new Smartphone(7, "Nokia", 70_000, "japane");
+
+        manedger.add(book1);
+        manedger.add(book2);
+        manedger.add(book3);
+        manedger.add(smartphone);
+
+        Product[] actual = manedger.searchBy("Nokia");
+
+        Product[] expected = {smartphone};
+
+        Assertions.assertEquals(expected.length, actual.length);
+
+    }
+    @Test
+    public void test10(){
+        ProductRepository repository = new ProductRepository();
+        ProductManedger manager = new ProductManedger(repository);
+        Smartphone smartphone = new Smartphone(7, "Nokia", 70_000, "japane");
+
+        manager.add(smartphone);
+
+        Product[] actual = manager.searchBy("Nokia");
+
+        Product[] expected = {smartphone};
+
+        Assertions.assertEquals(expected.length, actual.length);
+
+    }
+    @Test
+    public void test11(){
+        ProductRepository repository = new ProductRepository();
+        ProductManedger manager = new ProductManedger(repository);
+        Book book1 = new Book(1, "Lord of the Rings 1", 2_000, "", "");
+        Book book2 = new Book(2, "Lord of the Rings 2", 2_500, "", "");
+        Book book3 = new Book(3, "Rings", 2_600, "", "");
+        Book book4 = new Book(4, "Left", 2_700, "", "");
+
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        manager.add(book4);
+
+        Product[] actual = manager.searchBy("Left");
+
+        Product[] expected = {book4};
+
+        Assertions.assertEquals(expected.length, actual.length);
+
+    }
+
+}
---
+++ src/test/java/ProductRepositoryTest.java
@@ -0,0 +1,107 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.*;
+
+import org.junit.jupiter.api.Test;
+public class ProductRepositoryTest {
+    @Test
+    public void testRepositoryRemove() {
+        ProductRepository repository = new ProductRepository();
+
+        Book book1 = new Book(1, "Lord of the Rings 1", 2_000, "", "");
+        Book book2 = new Book(2, "Lord of the Rings 2", 2_500, "", "");
+        Book book3 = new Book(3, "Lord of the Rings 3", 2_600, "", "");
+
+        repository.add(book1);
+        repository.add(book2);
+        repository.add(book3);
+        repository.removeByID(2);
+
+        Product[] actual = repository.findAll();
+        Product[] expected = {book1, book3};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testRepositoryRemove1() {
+        ProductRepository repository = new ProductRepository();
+
+        Smartphone smartphone1 = new Smartphone(1, "Iphone", 50_000, "");
+        Smartphone smartphone2 = new Smartphone(2, "Lenovo", 30_000, "");
+        Smartphone smartphone3 = new Smartphone(3, "Samsung", 40_000, "");
+
+        repository.add(smartphone1);
+        repository.add(smartphone2);
+        repository.add(smartphone3);
+        repository.removeByID(3);
+
+        Product[] actual = repository.findAll();
+        Product[] expected = {smartphone1, smartphone2};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test1() {
+        ProductRepository repository = new ProductRepository();
+
+        Book book1 = new Book(1, "Lord of the Rings 1", 2_000, "a", "a");
+        Book book2 = new Book(2, "Lord of the Rings 2", 2_500, "b", "b");
+        Book book3 = new Book(3, "Lord of the Rings 3", 2_600, "c", "c");
+        Book book4 = new Book(4, "Lord of the Rings 4", 2_600, "d", "d");
+
+        repository.add(book1);
+        repository.add(book2);
+        repository.add(book3);
+        repository.add(book4);
+
+
+        Product[] actual = repository.findAll();
+        Product[] expected = {book1, book2, book3, book4};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+    @Test
+    public void test2() {
+        ProductRepository repository = new ProductRepository();
+
+        Book book1 = new Book(1, "Lord of the Rings 1", 2_000, "a", "a");
+        Book book2 = new Book(2, "Lord of the Rings 2", 2_500, "b", "b");
+        Book book3 = new Book(3, "Lord of the Rings 3", 2_600, "c", "c");
+        Book book4 = new Book(4, "Lord of the Rings 4", 2_600, "d", "d");
+
+        repository.add(book1);
+        repository.add(book2);
+        repository.add(book3);
+        repository.add(book4);
+
+        Assertions.assertThrows(NotFoundException.class, () -> repository.removeByID(5));
+
+
+        Product[] actual = repository.findAll();
+        Product[] expected = {book1, book2, book3, book4};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+    @Test
+    public void test3() {
+        ProductRepository repository = new ProductRepository();
+
+        Book book1 = new Book(1, "Lord of the Rings 1", 2_000, "a", "a");
+        Book book2 = new Book(2, "Lord of the Rings 2", 2_500, "b", "b");
+        Book book3 = new Book(3, "Lord of the Rings 3", 2_600, "c", "c");
+
+
+        repository.add(book1);
+        repository.add(book2);
+        repository.add(book3);
+
+
+        Assertions.assertThrows(NotFoundException.class,
+                () -> repository.removeByID(4));
+
+    }
+}
